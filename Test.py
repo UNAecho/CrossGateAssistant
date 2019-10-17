93a28a06e392ea23818a8c18d16a16f6de5c3786
andi.you <yadhr582855555@126.com>
2019-10-17 18:40:23 +0800

    add trainmodel backup

--- Test.py
+++ Test.py
@@ -1,30 +1,29 @@
 from time import sleep
 from PIL import ImageGrab
+import cv2
+from PIL import Image
+import os
 import pytesseract
 import win32api
 import time, datetime
 import psutil
 from repository import CharacterAttribute
-# while True:
-#     a = identify_find_template_or_not("map_east.png", 0.95)
-#     b = multi_template_coordinate("map_east.png", 0.95)
-#     print(a,b)
-#     # mouse_move(a[0],a[1])
-#     sleep(0.5)
+path = r"C:\study\CrossGateAssistant\ico\temp\train_ico"
 
-# from readContentOfScreen import read_number_of_screen
-# from readContentOfScreen import read_chi_of_screen
-# # a = identify_find_template_or_not("map_east.png", 0.95)
-# while True:
-#     # print("东："+read_number_of_screen(489,73,513,87),end="")
-#     # print("  南："+read_number_of_screen(599,73,619,87))
-#     print(read_number_of_screen(419,273,433,292))
-#     sleep(0.5)
-#
+
+for root, dirs, files in os.walk(path):
+    for file in files:
+        abs_path = os.path.join(root, file)
+        img = cv2.imread(abs_path)
+        # img = cv2.resize(im, (750, 130), interpolation=cv2.INTER_CUBIC)
+        # cv2.imwrite(root+"\\"+str(int(time.time()))+".png", img)
+        print("result : "+pytesseract.image_to_string(img,lang='num_money'))
+        # Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        sleep(0.3)
 # # win32api.SetCursorPos((1444,521))
 # # print(str(int(time.time())))
 
-while True:
-    a=ImageGrab.grab((393,225,468,238))
-    a.save("ico//temp//"+str(int(time.time()))+".png")
-    sleep(0.5)
+# while True:
+#     a=ImageGrab.grab((393,225,468,238))
+#     a.save("ico//temp//"+str(int(time.time()))+".png")
+#     sleep(0.5)
